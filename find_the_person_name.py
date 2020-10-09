d4d0c6b8ec58413ac96ccc9d743da7b3d2842983
AndyFungChen <68951255+AndyFungChen@users.noreply.github.com>
2020-10-10 01:00:02 +0800

    Update find_the_person_name.py

--- find_the_person_name.py
+++ find_the_person_name.py
@@ -2,12 +2,12 @@ import os
 import face_recognition
 from face_recognition.api import face_distance
 #name+path1 is the photo want to recognition, path is the database
-name = "-" #You can either use name+path1 to compound a path or copy full of the path into path1
-path1 = "-"+name+".jpg"
+name = "--" #You can either use name+path1 to compound a path or copy full of the path into path1
+path1 = "---"+name+".jpg"
 
-path = "-"     #database of face
-find = face_recognition.load_image_file(path1)
-find_encode = face_recognition.face_encodings(sample)[0]
+path = "---"     #database of face
+uk = face_recognition.load_image_file(path1)
+uk_encode = face_recognition.face_encodings(uk)[0]
 print(path1)
 
 known_names=[] 
@@ -16,11 +16,11 @@ for image_name in os.listdir(path):
         known_names.append(image_name.split(".")[0])
 for i in range(len(known_names)):
     path2 = path+known_names[i]+".jpg"
-    To_compare = face_recognition.load_image_file(path2)
+    data = face_recognition.load_image_file(path2)
     
-    unknown_face_encoding = face_recognition.face_encodings(To_compare)[0]
+    data_encoding = face_recognition.face_encodings(data)[0]
 
-    results = face_recognition.compare_faces([find_encode], unknown_face_encoding, tolerance=0.6)
+    results = face_recognition.compare_faces([uk_encode], data_encoding, tolerance=0.6)
     
     if False in results:
         pass
